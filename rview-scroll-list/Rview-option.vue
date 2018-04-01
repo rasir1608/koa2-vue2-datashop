ecb08f8cb92d8889a9f37c5803d60b1e2faf49ed
rasir <273581871@qq.com>
2018-04-02 00:13:28 +0800

    rview-scroll-list

---
+++ rview-scroll-list/Rview-option.vue
@@ -0,0 +1,37 @@
+<template>
+  <li :class="['rview-scroll-list-item',activeValue === item.value? 'is-active':'']" ref="rview-scroll-list-item" @click="sendValue">
+      <slot :item="item" :value="item.value" :label="item.label"></slot>
+      <span>{{activeValue === item.value}}/{{item.value}}/{{activeValue}}</span>
+  </li>
+</template>
+<script>
+export default {
+  props:{
+    item:{
+      type:Object,
+      default(){
+        return {}
+      }
+    },
+    value:null,
+    label:null,
+    activeValue:null,
+  },
+  data(){
+    return {
+      isActive:false,
+      selectValue:'',
+    }
+  },
+  watch:{
+  },
+  created(){
+  },
+  methods:{
+    sendValue(){
+      this.bus.$emit('rview-select-change',this.item)
+    },
+  }
+}
+</script>
+
